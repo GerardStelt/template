14b6fc1cc8f40b5671900bb71fad27bc479d08ab
Gerard van der Stelt <G.G.vanderStelt@Gmail.com>
2021-12-15 21:54:37 +0100

    commit

--- stencil.config.ts
+++ stencil.config.ts
@@ -12,7 +12,7 @@ export const config: Config = {
       // comment the following line to disable service workers in production
       serviceWorker: null,
       // baseUrl: 'https://myapp.local/',
-      baseUrl: 'https://gerardstelt.github.io/',
+      baseUrl: 'https://gerardstelt.github.io/template',
       dir: 'docs'
     },
   ],
