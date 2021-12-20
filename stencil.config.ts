4e1c0a20f7f9a9c73df8baa43685f016e8c9fdfd
Gerard van der Stelt <G.G.vanderStelt@Gmail.com>
2021-12-21 00:25:08 +0100

    Replaced Stencil with ion-pwa

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,19 +3,11 @@ import { Config } from '@stencil/core';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
+  globalStyle: 'src/global/app.css',
   taskQueue: 'async',
-  outputTargets: [
-    {
-      type: 'www',
-      // comment the following line to disable service workers in production
-      serviceWorker: null,
-      baseUrl: 'https://gerardstelt.github.io/',
-      dir: 'docs'
-    },
-  ],
-  devServer: {
-    openBrowser: false
-  }
+  outputTargets: [{
+    type: 'www',
+    serviceWorker: null
+  }],
 };
